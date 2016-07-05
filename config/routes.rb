95ce10f928db97ab93eeb74485735d1faf194812
Vicky Gray <vickygray26@hotmail.com>
2016-07-06 00:02:25 +0100

    Redirected root to /ideas path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :ideas
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root :to => redirect('/ideas')
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
